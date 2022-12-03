cad5b2af9b3304fa7604f259fc59259ade4fa559
Vladimir Samoylov <pingme@cageyv.dev>
2022-12-03 16:33:47 +0700

    cloudwatch_logs_retention_in_days default 14

--- variables.tf
+++ variables.tf
@@ -93,5 +93,5 @@ variable "recreate_missing_package" {
 variable "cloudwatch_logs_retention_in_days" {
   description = "Specifies the number of days you want to retain log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653."
   type        = number
-  default     = null
+  default     = 14
 }
