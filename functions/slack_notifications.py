1c4672f08f6bad3c9ef15dc118d99d5e2eac63f7
Anton Eremin <mobessona2@gmail.com>
2023-09-15 12:49:49 +0500

    fix(linting): fmt by ruff

--- functions/slack_notifications.py
+++ functions/slack_notifications.py
@@ -1,35 +1,40 @@
-import os
+import http.client
 import json
 import logging
-import http.client
+import os
+
 import boto3
 
-# ---------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
 # ENVIRONMENTAL VARIABLES
-# ---------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
 
 # Boolean flag, which determins if the incoming even should be printed to the output.
-LOG_EVENTS = os.getenv('LOG_EVENTS', 'False').lower() in ('true', '1', 't', 'yes', 'y')
+LOG_EVENTS = os.getenv("LOG_EVENTS", "False").lower() in ("true", "1", "t", "yes", "y")
 
-SLACK_WEBHOOK_URL_SOURCE_TYPE = os.getenv('SLACK_WEBHOOK_URL_SOURCE_TYPE', 'text').lower()
+SLACK_WEBHOOK_URL_SOURCE_TYPE = os.getenv(
+    "SLACK_WEBHOOK_URL_SOURCE_TYPE", "text"
+).lower()
 
-SLACK_WEBHOOK_URL = os.getenv('SLACK_WEBHOOK_URL', '')
-if SLACK_WEBHOOK_URL == '':
-    raise RuntimeError('The required env variable SLACK_WEBHOOK_URL is not set or empty!')
+SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL", "")
+if SLACK_WEBHOOK_URL == "":
+    raise RuntimeError(
+        "The required env variable SLACK_WEBHOOK_URL is not set or empty!"
+    )
 
 # Set the log level
 logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(os.environ.get("LOG_LEVEL", "INFO"))
 
-if SLACK_WEBHOOK_URL_SOURCE_TYPE == 'secretsmanager':
-    secretsmanager = boto3.client('secretsmanager')
+if SLACK_WEBHOOK_URL_SOURCE_TYPE == "secretsmanager":
+    secretsmanager = boto3.client("secretsmanager")
 
     secretsmanagerResponse = secretsmanager.get_secret_value(
-        SecretId = SLACK_WEBHOOK_URL,
+        SecretId=SLACK_WEBHOOK_URL,
     )
-    
-    SLACK_WEBHOOK_URL = secretsmanagerResponse['SecretString']
+
+    SLACK_WEBHOOK_URL = secretsmanagerResponse["SecretString"]
 
 # ---------------------------------------------------------------------------------------------------------------------
 # HELPER FUNCTIONS
@@ -41,184 +46,230 @@ if SLACK_WEBHOOK_URL_SOURCE_TYPE == 'secretsmanager':
 
 def ecs_events_parser(detail_type, detail):
     emoji_event_type = {
-        'ERROR': ':exclamation:',
-        'WARN': ':warning:',
-        'INFO': ':information_source:'
+        "ERROR": ":exclamation:",
+        "WARN": ":warning:",
+        "INFO": ":information_source:",
     }
     emoji_event_name = {
-        'SERVICE_DEPLOYMENT_IN_PROGRESS': ':arrows_counterclockwise:',
-        'SERVICE_DEPLOYMENT_COMPLETED': ':white_check_mark:',
-        'SERVICE_DEPLOYMENT_FAILED': ':x:'
+        "SERVICE_DEPLOYMENT_IN_PROGRESS": ":arrows_counterclockwise:",
+        "SERVICE_DEPLOYMENT_COMPLETED": ":white_check_mark:",
+        "SERVICE_DEPLOYMENT_FAILED": ":x:",
     }
     emoji_task_status = {
-        'PROVISIONING': ':clock1:',
-        'PENDING': ':clock6:',
-        'ACTIVATING': ':clock11:',
-        'RUNNING': ':up:',
-        'DEACTIVATING': ':arrow_backward:',
-        'STOPPING': ':rewind:',
-        'DEPROVISIONING': ':black_left_pointing_double_triangle_with_vertical_bar:',
-        'STOPPED': ':black_square_for_stop:'
+        "PROVISIONING": ":clock1:",
+        "PENDING": ":clock6:",
+        "ACTIVATING": ":clock11:",
+        "RUNNING": ":up:",
+        "DEACTIVATING": ":arrow_backward:",
+        "STOPPING": ":rewind:",
+        "DEPROVISIONING": ":black_left_pointing_double_triangle_with_vertical_bar:",
+        "STOPPED": ":black_square_for_stop:",
     }
 
-    if detail_type == 'ECS Container Instance State Change':
-        result = f'*Instance ID:* ' + detail['ec2InstanceId'] + '\n' + \
-                 '• Status: ' + detail['status']
-        if 'statusReason' in detail:
-            result = result + '\n' + '• Reason: ' + detail['statusReason']
+    if detail_type == "ECS Container Instance State Change":
+        result = (
+            "*Instance ID:* "
+            + detail["ec2InstanceId"]
+            + "\n"
+            + "• Status: "
+            + detail["status"]
+        )
+        if "statusReason" in detail:
+            result = result + "\n" + "• Reason: " + detail["statusReason"]
         return result
 
-    if detail_type == 'ECS Deployment State Change':
-        result = f'*Event Detail:*' + emoji_event_type.get(detail['eventType'], "") + emoji_event_name.get(detail['eventName'], "") + '\n' + \
-                 '• ' + detail['eventType'] + ' - ' + detail['eventName'] + '\n' + \
-                 '• Deployment: ' + detail['deploymentId'] + '\n' + \
-                 '• Reason: ' + detail['reason']
+    if detail_type == "ECS Deployment State Change":
+        result = (
+            "*Event Detail:*"
+            + emoji_event_type.get(detail["eventType"], "")
+            + emoji_event_name.get(detail["eventName"], "")
+            + "\n"
+            + "• "
+            + detail["eventType"]
+            + " - "
+            + detail["eventName"]
+            + "\n"
+            + "• Deployment: "
+            + detail["deploymentId"]
+            + "\n"
+            + "• Reason: "
+            + detail["reason"]
+        )
         return result
 
-    if detail_type == 'ECS Service Action':
-        result = f'*Event Detail:*' + emoji_event_type.get(detail['eventType'], "") + emoji_event_name.get(detail['eventName'], "") + '\n' + \
-                 '• ' + detail['eventType'] + ' - ' + detail['eventName']
-        if 'capacityProviderArns' in detail:
+    if detail_type == "ECS Service Action":
+        result = (
+            "*Event Detail:*"
+            + emoji_event_type.get(detail["eventType"], "")
+            + emoji_event_name.get(detail["eventName"], "")
+            + "\n"
+            + "• "
+            + detail["eventType"]
+            + " - "
+            + detail["eventName"]
+        )
+        if "capacityProviderArns" in detail:
             capacity_providers = ""
-            for capacity_provider in detail['capacityProviderArns']:
+            for capacity_provider in detail["capacityProviderArns"]:
                 try:
-                    capacity_providers = capacity_providers + capacity_provider.split(':')[5].split('/')[1] + ", "
+                    capacity_providers = (
+                        capacity_providers
+                        + capacity_provider.split(":")[5].split("/")[1]
+                        + ", "
+                    )
                 except Exception:
-                    log.error('Error parsing clusterArn: `{}`'.format(capacity_provider))
+                    log.error(
+                        "Error parsing clusterArn: `{}`".format(capacity_provider)
+                    )
                     capacity_providers = capacity_providers + capacity_provider + ", "
             if capacity_providers != "":
-                result = result + '\n' + '• Capacity Providers: ' + capacity_providers
+                result = result + "\n" + "• Capacity Providers: " + capacity_providers
         return result
 
-    if detail_type == 'ECS Task State Change':
+    if detail_type == "ECS Task State Change":
         container_instance_id = "UNKNOWN"
-        if 'containerInstanceArn' in detail:
+        if "containerInstanceArn" in detail:
             try:
-                container_instance_id = detail['containerInstanceArn'].split(':')[5].split('/')[2]
+                container_instance_id = (
+                    detail["containerInstanceArn"].split(":")[5].split("/")[2]
+                )
             except Exception:
-                log.error('Error parsing containerInstanceArn: `{}`'.format(detail['containerInstanceArn']))
-                container_instance_id = detail['containerInstanceArn']
+                log.error(
+                    "Error parsing containerInstanceArn: `{}`".format(
+                        detail["containerInstanceArn"]
+                    )
+                )
+                container_instance_id = detail["containerInstanceArn"]
         try:
-            task_definition = detail['taskDefinitionArn'].split(':')[5].split(
-                '/')[1] + ":" + detail['taskDefinitionArn'].split(':')[6]
+            task_definition = (
+                detail["taskDefinitionArn"].split(":")[5].split("/")[1]
+                + ":"
+                + detail["taskDefinitionArn"].split(":")[6]
+            )
         except Exception:
-            log.error('Error parsing taskDefinitionArn: `{}`'.format(detail['taskDefinitionArn']))
-            task_definition = detail['taskDefinitionArn']
+            log.error(
+                "Error parsing taskDefinitionArn: `{}`".format(
+                    detail["taskDefinitionArn"]
+                )
+            )
+            task_definition = detail["taskDefinitionArn"]
         try:
-            task = detail['taskArn'].split(':')[5].split('/')[2]
+            detail["taskArn"].split(":")[5].split("/")[2]
         except Exception:
-            log.error('Error parsing taskArn: `{}`'.format(detail['taskArn']))
-            task = detail['taskArn']
-        result = f'*Event Detail:* ' + '\n' + \
-                 '• Task Definition: ' + task_definition + '\n' + \
-                 '• Last: ' + detail['lastStatus'] + ' ' + emoji_task_status.get(detail['lastStatus'], "") + '\n' + \
-                 '• Desired: ' + detail['desiredStatus'] + ' ' + emoji_task_status.get(detail['desiredStatus'], "")
+            log.error("Error parsing taskArn: `{}`".format(detail["taskArn"]))
+            detail["taskArn"]
+        result = (
+            "*Event Detail:* "
+            + "\n"
+            + "• Task Definition: "
+            + task_definition
+            + "\n"
+            + "• Last: "
+            + detail["lastStatus"]
+            + " "
+            + emoji_task_status.get(detail["lastStatus"], "")
+            + "\n"
+            + "• Desired: "
+            + detail["desiredStatus"]
+            + " "
+            + emoji_task_status.get(detail["desiredStatus"], "")
+        )
         if container_instance_id != "UNKNOWN":
-            result = result + '\n' + '• Instance ID: ' + container_instance_id
-        if detail['lastStatus'] == 'RUNNING':
-            if 'healthStatus' in detail:
-                result = result + '\n' + '• HealthStatus: ' + detail['healthStatus']
-        if detail['lastStatus'] == 'STOPPED':
-            if 'stopCode' in detail:
-                result = result + '\n' + ':bangbang: Stop Code: ' + detail['stopCode']
-            if 'stoppedReason' in detail:
-                result = result + '\n' + ':bangbang: Stop Reason: ' + detail['stoppedReason']
-            if 'containers' in detail:
-                result = result + '\n' + 'Task containers and their exit code:'
-                for container in detail['containers']:
-                    result = result + '\n' + ' - ' + container['name'] + ': ' + str(container.get('exitCode', 'unknown'))
+            result = result + "\n" + "• Instance ID: " + container_instance_id
+        if detail["lastStatus"] == "RUNNING":
+            if "healthStatus" in detail:
+                result = result + "\n" + "• HealthStatus: " + detail["healthStatus"]
+        if detail["lastStatus"] == "STOPPED":
+            if "stopCode" in detail:
+                result = result + "\n" + ":bangbang: Stop Code: " + detail["stopCode"]
+            if "stoppedReason" in detail:
+                result = (
+                    result + "\n" + ":bangbang: Stop Reason: " + detail["stoppedReason"]
+                )
+            if "containers" in detail:
+                result = result + "\n" + "Task containers and their exit code:"
+                for container in detail["containers"]:
+                    result = (
+                        result
+                        + "\n"
+                        + " - "
+                        + container["name"]
+                        + ": "
+                        + str(container.get("exitCode", "unknown"))
+                    )
         return result
 
-    return f'*Event Detail:* ```{json.dumps(detail, indent=4)}```'
+    return f"*Event Detail:* ```{json.dumps(detail, indent=4)}```"
 
 
 # Input: EventBridge Message
 # Output: Slack Message
 def event_to_slack_message(event):
-    event_id = event.get('id')
-    detail_type = event.get('detail-type')
-    account = event.get('account')
-    time = event.get('time')
-    region = event.get('region')
+    event_id = event.get("id")
+    detail_type = event.get("detail-type")
+    account = event.get("account")
+    time = event.get("time")
+    region = event.get("region")
     resources = []
-    for resource in event['resources']:
+    for resource in event["resources"]:
         try:
-            resources.append(":dart: " + resource.split(':')[5])
+            resources.append(":dart: " + resource.split(":")[5])
         except Exception:
-            log.error('Error parsing the resource ARN: `{}`'.format(resource))
+            log.error("Error parsing the resource ARN: `{}`".format(resource))
             resources.append(":dart: " + resource)
-    detail = event.get('detail')
+    detail = event.get("detail")
     known_detail = ecs_events_parser(detail_type, detail)
-    blocks = list()
-    contexts = list()
-    title = f'*{detail_type}*'
-    blocks.append(
-        {
-            'type': 'section',
-            'text': {
-                'type': 'mrkdwn',
-                'text': title
-            }
-        }
-    )
+    blocks = []
+    contexts = []
+    title = f"*{detail_type}*"
+    blocks.append({"type": "section", "text": {"type": "mrkdwn", "text": title}})
     if resources:
         blocks.append(
             {
-                'type': 'section',
-                'text': {
-                    'type': 'mrkdwn',
-                    'text': "*Resources*:\n" + '\n'.join(resources)
-                }
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Resources*:\n" + "\n".join(resources),
+                },
             }
         )
     if detail and not known_detail:
         blocks.append(
             {
-                'type': 'section',
-                'text': {
-                    'type': 'mrkdwn',
-                    'text': f'*Event Detail:* ```{json.dumps(detail, indent=4)}```'
-                }
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"*Event Detail:* ```{json.dumps(detail, indent=4)}```",
+                },
             }
         )
     if known_detail:
         blocks.append(
-            {
-                'type': 'section',
-                'text': {
-                    'type': 'mrkdwn',
-                    'text': known_detail
-                }
-            }
+            {"type": "section", "text": {"type": "mrkdwn", "text": known_detail}}
         )
-    contexts.append({
-        'type': 'mrkdwn',
-        'text': f'Account: {account} Region: {region}'
-    })
-    contexts.append({
-        'type': 'mrkdwn',
-        'text': f'Time: {time} UTC Id: {event_id}'
-    })
-    blocks.append({
-        'type': 'context',
-        'elements': contexts
-    })
-    blocks.append({'type': 'divider'})
-    return {'blocks': blocks}
+    contexts.append({"type": "mrkdwn", "text": f"Account: {account} Region: {region}"})
+    contexts.append({"type": "mrkdwn", "text": f"Time: {time} UTC Id: {event_id}"})
+    blocks.append({"type": "context", "elements": contexts})
+    blocks.append({"type": "divider"})
+    return {"blocks": blocks}
 
 
 # Slack web hook example
 # https://hooks.slack.com/services/XXXXXXX/XXXXXXX/XXXXXXXXXX
 def post_slack_message(hook_url, message):
-    log.debug(f'Sending message: {json.dumps(message, indent=4)}')
-    headers = {'Content-type': 'application/json'}
-    connection = http.client.HTTPSConnection('hooks.slack.com')
-    connection.request('POST',
-                       hook_url.replace('https://hooks.slack.com', ''),
-                       json.dumps(message),
-                       headers)
+    log.debug(f"Sending message: {json.dumps(message, indent=4)}")
+    headers = {"Content-type": "application/json"}
+    connection = http.client.HTTPSConnection("hooks.slack.com")
+    connection.request(
+        "POST",
+        hook_url.replace("https://hooks.slack.com", ""),
+        json.dumps(message),
+        headers,
+    )
     response = connection.getresponse()
-    log.debug('Response: {}, message: {}'.format(response.status, response.read().decode()))
+    log.debug(
+        "Response: {}, message: {}".format(response.status, response.read().decode())
+    )
     return response.status
 
 
@@ -229,7 +280,7 @@ def post_slack_message(hook_url, message):
 
 def lambda_handler(event, context):
     if LOG_EVENTS:
-        log.info('Event logging enabled: `{}`'.format(json.dumps(event)))
+        log.info("Event logging enabled: `{}`".format(json.dumps(event)))
 
     if event.get("source") != "aws.ecs":
         raise ValueError('The source of the incoming event is not "aws.ecs"')
@@ -238,13 +289,15 @@ def lambda_handler(event, context):
     response = post_slack_message(SLACK_WEBHOOK_URL, slack_message)
     if response != 200:
         log.error(
-            "Error: received status `{}` using event `{}` and context `{}`".format(response, event,
-                                                                                   context))
+            "Error: received status `{}` using event `{}` and context `{}`".format(
+                response, event, context
+            )
+        )
     return json.dumps({"code": response})
 
 
 # For local testing
-if __name__ == '__main__':
-    with open('./test/eventbridge_event.json') as f:
+if __name__ == "__main__":
+    with open("./test/eventbridge_event.json") as f:
         test_event = json.load(f)
     lambda_handler(test_event, "default_context")
