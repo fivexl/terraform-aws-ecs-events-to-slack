0a411fee94906e7786de774a49e39bbb16fd8436
Simon Tingle <tingleteaching@gmail.com>
2026-01-22 16:45:55 +0000

    chore: remove legacy workflows and fix duplicate vars

--- deploy_iam.tf
+++ deploy_iam.tf
@@ -1,9 +1,3 @@
-# This variable is needed to configure the trust policy
-variable "github_repo" {
-  description = "The GitHub repository in format 'org/repo' (e.g., 'fivexl/ecs-events-to-slack')"
-  type        = string
-}
-
 # The OIDC provider is assumed to exist in the account already.
 # We reference it by constructing the ARN standard for GitHub.
 data "aws_iam_openid_connect_provider" "github" {
