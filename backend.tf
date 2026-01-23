751ee4308390e5b6c75179eed9837e3d4c528f3f
Simon Tingle <tingleteaching@gmail.com>
2026-01-23 15:55:53 +0000

    chore: remove hardcoded backend and unused script - backend.tf was deleted so other people could use or clone this script

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-a6490666acaa9e18f19bdc1559e7c3acde30c9de"
-    key            = "simon-tingle/terraform-aws-ecs-events-to-slack.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-state-lock-a6490666acaa9e18f19bdc1559e7c3acde30c9de"
-    encrypt        = true
-  }
-}
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from terraform-aws-ecs-events-to-slack!")
-
-
-if __name__ == "__main__":
-    main()
